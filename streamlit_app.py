75a9ef4b76a1f1cdfd4953229932777d2e83a23c
Mertali Muhsin Tercan <168694741+mertalitercan@users.noreply.github.com>
2025-08-30 02:55:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
-st.title('ML App - Mertali Tercan')
+st.title("ML App - Mertali Tercan")
 
-st.info('Welcome')
+st.info("Welcome")
+
+data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")

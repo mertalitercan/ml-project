de744f7397fbd86e9c1b373fb70256bd50bac12b
Mertali Muhsin Tercan <168694741+mertalitercan@users.noreply.github.com>
2025-08-30 03:29:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.title("ML App - Mertali Tercan")
+st.title("Machine Learning Application - Mertali Tercan")
+st.info("Hello!")
 
-st.info("Welcome")
+data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+for animal in data:
+  animal.body_mass_g /= 1000
+
+data.rename(columns={'body_mass_g' : 'body_mass_kg'}, inplace='True')
 
 with st.expander('Data:'):
   st.write("**Raw data**")
-  data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   data
 
   st.write('**X**')
@@ -18,9 +24,6 @@ with st.expander('Data:'):
   y = data.species
   y
 
-st.markdown('hey')
-st.markdown('hey')
-
-with st.expander('Visualize the data:'):
+with st.expander('Visualize:'):
   st.scatter_chart(data=data, x='bill_depth_mm', x_label='Bill Depth', y='body_mass_g', y_label='Body Mass', color='species')
 

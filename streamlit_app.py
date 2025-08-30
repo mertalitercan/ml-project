4110a4e5069a497308a304986c4478685888451e
Mertali Muhsin Tercan <168694741+mertalitercan@users.noreply.github.com>
2025-08-30 03:00:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,16 @@ st.title("ML App - Mertali Tercan")
 
 st.info("Welcome")
 
-data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-data
+with st.expander('Data:'):
+  st.write("**Raw data**")
+  data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  data
+
+  st.write('**X**')
+  X = data.drop(['species'])
+  X
+
+  st.write('**Y**')
+  y = data['species']
+  y
 

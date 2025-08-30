9a7dda907ad9c79c0d926aca5f751877084952ff
Mertali Muhsin Tercan <168694741+mertalitercan@users.noreply.github.com>
2025-08-30 03:56:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,7 @@ with st.sidebar:
   island = st.selectbox('Island', ('Torgersen', 'Biscoe', 'Dream'), placeholder='Choose island...')
   gender = st.selectbox('Gender', ('Male', 'Female'), placeholder='Choose gender...')
   bill_length_mm = st.slider('Bill Length (mm)', min(data['bill_length_mm']), max(data['bill_length_mm']), data['bill_length_mm'].mean())
+  bill_depth_mm = st.slider('Bill Depth (mm)', min(data['bill_depth_mm']), max(data['bill_depth_mm']), data['bill_depth_mm'].mean())
+  flipper_length_mm = st.slider('Flipper Length (mm)', min(data['flipper_length_mm']), max(data['flipper_length_mm'), data['flipper_length_mm'].mean())
+  body_mass_kg = st.slider('Body Mass (kg)', min(data['body_mass_kg']), max(data['body_mass_kg']), data['body_mass_kg'].mean())
 

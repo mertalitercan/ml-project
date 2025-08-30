a115949f273e14480f3940b0af56f07c59cdd6c5
Mertali Muhsin Tercan <168694741+mertalitercan@users.noreply.github.com>
2025-08-30 03:49:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import math
 
 st.title("Machine Learning Application - Mertali Tercan")
 st.info("Hello!")
@@ -24,5 +25,11 @@ with st.expander('Data:'):
   y
 
 with st.expander('Visualize:'):
-  st.scatter_chart(data=data, x='bill_depth_mm', x_label='Bill Depth', y='body_mass_kg', y_label='Body Mass', color='species')
+  st.scatter_chart(data=data, x='bill_depth_mm', x_label='Bill Depth (mm)', y='body_mass_kg', y_label='Body Mass (kg)', color='species')
+
+with st.sidebar:
+  st.header('Your inputs:')
+  island = st.selectbox('Island', ('Torgersen', 'Biscoe', 'Dream'), placeholder='Choose island...')
+  gender = st.selectbox('Gender', ('Male', 'Female'), placeholder='Choose gender...')
+  bill_length_mm = st.slider('Bill Length (mm)', min(data['bill_length_mm'], max(data['bill_length_mm'], mean(data['bill_length_mm']
 
